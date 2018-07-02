3f6acd13e2af7a450f982e82c1b35458ffe06f00
Jefferson Lima <jeffersonlimasantos23@gmail.com>
2018-07-01 21:20:24 -0300

    Auth and deploy

--- BillingCycleFront/app/config/consts.js
+++ BillingCycleFront/app/config/consts.js
@@ -4,7 +4,9 @@ angular.module('primeiraApp').constant('consts', {
   owner: 'Cod3r',
   year: '2017',
   site: 'http://cod3r.com.br',
-  apiUrl: 'http://localhost:3003/api',
+  apiUrl: 'https://jef-my-money-app-backend.herokuapp.com/api',
+  oapiUrl:'https://jef-my-money-app-backend.herokuapp.com/oapi',
+  userKey:'_primeira_app_user',
 }).run(['$rootScope', 'consts', function($rootScope, consts) {
   $rootScope.consts = consts
 }])
