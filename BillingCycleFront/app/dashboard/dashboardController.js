7fad4491d983726796eb6b1ed50e47e6dc3b322c
Jefferson Lima <jeffersonlimasantos23@gmail.com>
2018-07-07 00:36:29 -0300

    Correção de chamada sumario

--- BillingCycleFront/app/dashboard/dashboardController.js
+++ BillingCycleFront/app/dashboard/dashboardController.js
@@ -7,7 +7,7 @@ angular.module('primeiraApp').controller('DashboardCtrl', [
 function DashboardController($http, consts) {
   const vm = this
   vm.getSummary = function() {
-    const url = `${consts.apiUrl}/billingSummary`;
+    const url = `${consts.apiUrl}/billingCycles/summary`;
     $http.get(url).then(function(response) {
       const {credit = 0, debt = 0} = response.data
       vm.credit = credit
